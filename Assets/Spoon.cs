f8093ea3de59ed481b9bf2f6f337a1edba652b35
jpparent <jeanp.parent@gmail.com>
2019-08-03 00:22:04 -0400

    Initial commit

---
+++ Assets/Spoon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spoon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// update position to where mouse is
+		Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+		RaycastHit hitInfo;
+		if (Physics.Raycast(ray, out hitInfo ) )
+		{
+			if (hitInfo.transform != transform )
+			{
+				transform.position = hitInfo.point;
+			}
+		}
+    }
+}
